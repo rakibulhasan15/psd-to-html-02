c718991e0df5ec9301cf5a11af47d73785656486
MD. RAKIBUL HASAN <79636944+rakibulhasan15@users.noreply.github.com>
2021-08-11 10:42:00 +0600

    add html and css file for psd file.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// ================ jQuery bxSlider ===================
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('#main_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+  });
